4b2cc8fe0ebd72306268528645f4e22f97a5d896
FelixShih <zx8714245@gmail.com>
2024-07-10 09:42:49 +0800

    feat: modify the trash display

--- src/app/dashboard/trash/page.tsx
+++ src/app/dashboard/trash/page.tsx
@@ -1,7 +1,7 @@
 import { getDeletedTodos, restoreTodo } from "@/actions/todo"
 import TodoDisplay from "@/components/dashboard/TodoDisplay"
+import TrashTodoDisplay from "@/components/dashboard/TrashTodoDisplay";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface DashboardTrashPageProps {}
 
@@ -18,39 +18,16 @@ const DashboardTrashPage = async ({}: DashboardTrashPageProps) => {
   }
 
   const deletedTodos = response.data;
-  
-  const handleRestore = async (id: string) => {
-    const response = await restoreTodo(id);
-    if (response.status === "success") {
-      
-    } else {
-    }
-  };
 
   return (
-    <div className="p-4">
+    <section className="w-full min-h-screen flex flex-col space-y-2 p-2">
       <h1 className="text-2xl font-bold mb-4">Trash</h1>
       {deletedTodos.length === 0 ? (
-        <p>The trash is</p>
+        <p>The trash is empty</p>
       ) : (
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {deletedTodos.map((todo) => (
-            <Card key={todo.id}>
-              <CardHeader>
-                <CardTitle>{todo.title}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Button 
-                  className="mt-2" 
-                >
-                  恢復
-                </Button>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+        <TrashTodoDisplay deletedTodos={deletedTodos} />
       )}
-    </div>
+    </section>
   )
 }
 
---
+++ src/components/dashboard/TrashTodoDisplay.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Todo, restoreTodo } from "@/actions/todo";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+interface TrashTodoDisplayProps {
+  deletedTodos: Todo[];
+}
+
+const TrashTodoDisplay: React.FC<TrashTodoDisplayProps> = ({deletedTodos}) => {
+  const handleRestore = async (id: string) => {
+    const response = await restoreTodo(id);
+    if (response.status !== "success") {
+      console.error("restore failed")
+    }
+  };
+
+  return (
+    <>
+          {deletedTodos.map((todo) => (
+            <Card key={todo.id}className="w-full p-2">
+              <CardContent className="flex flex-row items-center justify-between">
+                <CardTitle>{todo.title}</CardTitle>
+                <Button 
+                  type="button" 
+                  onClick={() => {
+                    handleRestore(todo.id);
+                  }}
+                >
+                  恢復
+                </Button>
+              </CardContent>
+            </Card>
+          ))}
+        </>
+  )
+}
+
+export default TrashTodoDisplay
