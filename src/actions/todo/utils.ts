30cd239699e60dba0b0b5e743c1ade5d0685411d
FelixShih <zx8714245@gmail.com>
2024-11-24 00:23:24 +0800

    feat: create the reminder field with todo

--- src/actions/todo/utils.ts
+++ src/actions/todo/utils.ts
@@ -4,6 +4,18 @@ import { getUser } from "@/lib/lucia";
 import { db } from "@/lib/prisma";
 import { z } from "zod";
 
+export type ReminderSchedule = {
+  id: string;
+  isEnabled: boolean;
+  startTime: Date;
+  repeatType?: 'NONE' | 'DAILY' | 'WEEKLY' | 'MONTHLY';
+  repeatDays?: string;
+  repeatDate?: number;
+  repeatStart?: Date;
+  repeatEnd?: Date;
+  lastTriggered?: Date;
+};
+
 export type Todo = {
   id: string;
   title: string;
@@ -13,6 +25,7 @@ export type Todo = {
   endDate?: Date;
   labels?: string[];
   subTodos?: Todo[];
+  reminder?: ReminderSchedule;
 };
 
 export type ApiResponse<T> = {
@@ -21,6 +34,16 @@ export type ApiResponse<T> = {
   data: T[];
 };
 
+export const ReminderSchema = z.object({
+  isEnabled: z.boolean().default(true),
+  startTime: z.date(),
+  repeatType: z.enum(['NONE', 'DAILY', 'WEEKLY', 'MONTHLY']).optional(),
+  repeatDays: z.string().optional(),
+  repeatDate: z.number().min(1).max(31).optional(),
+  repeatStart: z.date().optional(),
+  repeatEnd: z.date().optional(),
+});
+
 export const TodoSchema = z.object({
   title: z.string().min(1, "標題不能為空").max(100, "標題不能超過100個字符"),
   description: z.string().max(300, '描述不能超過300個字符').optional(),
@@ -28,6 +51,7 @@ export const TodoSchema = z.object({
   endDate: z.date().optional(),
   isDone: z.boolean().optional(),
   labels: z.array(z.string()).optional(),
+  reminder: ReminderSchema.optional(),
 });
 
 export async function handleUserAuth(): Promise<{ dbUser: any } | { error: ApiResponse<Todo> }> {
@@ -50,7 +74,8 @@ export function formatTodo(todo: any): Todo {
     description: todo.description ?? undefined,
     startDate: todo.startDate ?? undefined,
     endDate: todo.endDate ?? undefined,
-    labels: todo.labels ? todo.labels.map((label: any) => label.name) : undefined
+    labels: todo.labels ? todo.labels.map((label: any) => label.name) : undefined,
+    reminder: todo.reminder ?? undefined
   };
 }
 
